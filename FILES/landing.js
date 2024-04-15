a47ea49f54f005abc85f7a9e099386049e099f80
CLINTON <issnasty2@gmail.com>
2024-04-15 11:13:51 +0300

    final commit

--- FILES/landing.js
+++ FILES/landing.js
@@ -1,39 +1,52 @@
-// Define Base URL fo(r API requests
-document.addEventListener("DOMContentLoaded", () => {
-    const baseUrl = "http://localhost:5000/events";
-
-    // function to fetch event details and update UI
-    const fechAndUpdateUi = async(eventsId) => {
-        try{
-            const response = await fetch(`${baseUrl}/events/${eventsId}`);
-            if (!response.ok) {
-                throw new Error("Failed to fetch event details");
-            }
-            const eventData = await response.json();
-            const {
-                id,
-                name,
-                venue,
-                price,
-                description,
-                poster,
-            } = eventData;
-
-            // Calculate available tickets
-            const availableTickets = capacity - ticketSold;
-
-            //  Update DOM elements with event details
-            document.getElementsByTagName("img").src = poster;
-            
-
-        }
-    }
-})
 
 
+// DEFINE BASE URL FOR API REQUESTS
 
-const button = document.querySelector("button");
+const baseUrl = "http://localhost:5001/events"
 
-button.addEventListener("click",function pay() {
-    fetch
-})
+
+// Get references to the button and the popup
+const buttons = document.querySelectorAll('.button');
+const popup = document.querySelector('.popup');
+
+// Function to open the popup
+function openPopup() {
+  popup.classList.add('open-popup');
+}
+
+// Function to close the popup
+function closePopup() {
+  popup.classList.remove('open-popup');
+}
+
+// Add click event listeners to each button
+buttons.forEach(button => {
+  button.addEventListener('click', openPopup);
+});
+
+// Add click event listener to close button in popup
+document.getElementById('close').addEventListener('click', closePopup);
+
+
+const confirmPayment = document.getElementById("confirm");
+
+function generateQr() {
+    // Get the text content of the card-title element
+    let qrText = document.querySelector(".card-title").textContent;
+
+    // Generate the QR code URL with the text content
+    let qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(qrText)}`;
+
+    // Open the generated QR code URL in a new tab
+    window.open(qrCodeUrl);
+}
+
+// Function to switch tabs
+function navigateToPage(pageUrl) {
+    window.location.href = pageUrl;
+}
+
+// Add click event listener to the confirm payment button
+confirmPayment.addEventListener("click", function() {
+    navigateToPage("http://127.0.0.1:3000/FILES/qrCode.page/qrCode.html");
+});
